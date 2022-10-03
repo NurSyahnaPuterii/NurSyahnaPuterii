aaad140dad2078d80c6e2ca382fd7ae489fe2e0d
Nursyahna Puteri <114853207+NurSyahnaPuterii@users.noreply.github.com>
2022-10-03 21:09:34 +0800

    Add files via upload

---
+++ PRAK002-2210817120007-NursyahnaPuteri.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main () { 
+    printf ("Selamat Pagi, Nursyahna Puteri\n");
+    printf ("Selamat Siang, Nursyahna Puteri\n");
+    printf ("Selamat Sore, Nursyahna Puteri\n");
+}
