252aaf25367e7012d69acce31fcb626b2be98f77
Nursyahna Puteri <114853207+NurSyahnaPuterii@users.noreply.github.com>
2022-10-03 21:15:09 +0800

    Add files via upload

---
+++ PRAK004-2210817120007-NursyahnaPuteri.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main () {
+    printf ("#############################################\n");
+    printf ("#                                           #\n");
+    printf ("#           Nursyahna Puteri                #\n");
+    printf ("#            2210817120007                  #\n");
+    printf ("#                                           #\n");
+    printf ("#############################################\n");
+}
